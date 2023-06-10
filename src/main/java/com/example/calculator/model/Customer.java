9a860d4a586ad883a2817a873dab7d6f0d5705b5
huutrungu <huutrungg02@gmail.com>
2023-06-10 09:25:57 +0700

    trungmini customer

---
+++ src/main/java/com/example/calculator/model/Customer.java
@@ -0,0 +1,49 @@
+package com.example.calculator.model;
+
+public class Customer {
+    private int id;
+    private String name;
+    private boolean gender;
+
+    public Customer() {
+    }
+
+    public Customer(int id, String name, boolean gender) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isGender() {
+        return gender;
+    }
+
+    public void setGender(boolean gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", gender=" + gender +
+                '}';
+    }
+}
